d4e75ab928db421d11e35a6505d405788368cb42
Nick Shterbina <nick.shterbina04@gmail.com>
2025-08-04 22:43:55 +0300

    Set up environment for future work

---
+++ src/main/java/ai/carmatch/CarMatchAiApplication.java
@@ -0,0 +1,13 @@
+package ai.carmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMatchAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMatchAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/carmatch/CarMatchAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.carmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMatchAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
